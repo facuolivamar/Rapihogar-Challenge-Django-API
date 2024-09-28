6bd084d77333de8ebb03b64a5a653002d0fb602d
facuolivamar <facundoolivam@gmail.com>
2024-09-28 00:15:29 -0300

    admin register db models

--- rapihogar/admin.py
+++ rapihogar/admin.py
@@ -1,1 +1,8 @@
-""" Register models """
+from django.contrib import admin
+from .models import User, Company, Tecnico, Scheme, Pedido
+
+admin.site.register(User)
+admin.site.register(Tecnico)
+admin.site.register(Scheme)
+admin.site.register(Company)
+admin.site.register(Pedido)

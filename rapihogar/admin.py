40cc6d370fff2a4b5603c55f42b25390bca13ed0
facuolivamar <facundoolivam@gmail.com>
2024-09-28 00:17:24 -0300

    admin list_display and list_filter

--- rapihogar/admin.py
+++ rapihogar/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
 from .models import User, Company, Tecnico, Scheme, Pedido
 
-admin.site.register(User)
-admin.site.register(Tecnico)
-admin.site.register(Scheme)
-admin.site.register(Company)
-admin.site.register(Pedido)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "full_name", "email")
+    list_filter = ["is_active", "is_staff"]
+
+class TecnicoAdmin(admin.ModelAdmin):
+    list_display = ("id", "full_name", )
+
+class SchemeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    list_filter = ["name"]
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "email", "phone", "website")
+
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ("id", "type_request", "scheme__name")
+    list_filter = ["type_request", "scheme__name"]
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Tecnico, TecnicoAdmin)
+admin.site.register(Scheme, SchemeAdmin)
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Pedido, PedidoAdmin)

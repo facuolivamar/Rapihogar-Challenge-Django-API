31c3b584b97c939f36665a720121ecce8fbe00a2
facuolivamar <facundoolivam@gmail.com>
2024-09-28 15:07:10 -0300

    api/urls mods

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,19 @@
-from rapihogar.models import Company
 from rest_framework import routers
-from django.urls import path, include
-from .views import CompanyViewSet, TecnicoListView, TecnicoReportView, PedidoUpdateView
+from django.urls import path
+from .resources.company import CompanyViewSet
+from .resources.pedido import PedidoUpdateView
+from .resources.tecnico import TecnicoListView, TecnicoReportView
 
 router = routers.DefaultRouter()
 router.register(r'company', CompanyViewSet, basename='company')
 
 
 urlpatterns = router.urls + [
+    path(
+        "pedido/<int:pk>",
+        PedidoUpdateView.as_view(),
+        name="pedido",
+    ),
     path(
         "tecnico/",
         TecnicoListView.as_view(),
@@ -17,10 +23,5 @@ urlpatterns = router.urls + [
         "tecnico/informe",
         TecnicoReportView.as_view(),
         name="tecnico/informe",
-    ),
-    path(
-        "pedido/<int:pk>",
-        PedidoUpdateView.as_view(),
-        name="pedido",
     ),
 ]

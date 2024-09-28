0dce1b085b8c0cbaa18e3f902298cc7bfefec16c
facuolivamar <facundoolivam@gmail.com>
2024-09-28 14:02:00 -0300

    Tests for the new services: Tecnico, Pedido + removed permission ".IsAuthenticatedOrReadOnly"

--- rapihogar/settings.py
+++ rapihogar/settings.py
@@ -128,9 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',  # Para la exploración automática
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
-    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticatedOrReadOnly"],
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
